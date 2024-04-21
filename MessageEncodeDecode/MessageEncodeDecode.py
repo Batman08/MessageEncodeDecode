d307ecb0f2c7eaad169cd3faa7b8c219a49f4e2d
bilal asghar <asgharbilal8@gmail.com>
2024-04-21 15:32:10 +0100

    something....

--- MessageEncodeDecode/MessageEncodeDecode.py
+++ MessageEncodeDecode/MessageEncodeDecode.py
@@ -1,2 +1,13 @@
-from tkinter import *
+import tkinter as tk
 import base64
+
+#initialize window
+
+WINDOW = tk.Tk()
+
+WINDOW.geometry('500x300')
+WINDOW.resizable(0,0)
+WINDOW.title("WeSellAnyCode - Message Encode Decode")
+
+#tk.Label(WINDOW, text ='ENCODE DECODE', font = 'arial 20 bold').pack()
+#tk.Label(WINDOW, text ='DataFlair', font = 'arial 20 bold').pack(side = BOTTOM)
